be76795d8e9ae6aa42491ea67f67c8c2223e89d6
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-01 23:04:20 -0400

    added a add campground functionality

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -16,20 +16,32 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
-})
+});
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index.ejs', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show.ejs', { campground });
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-}) 
+});
