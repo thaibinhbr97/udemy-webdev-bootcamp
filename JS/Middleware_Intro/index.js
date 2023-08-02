2635c3df75ba7949a6cebe948380df60b52c0365
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-02 06:38:53 -0400

    added a morgan logger middleware

---
+++ JS/Middleware_Intro/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('HOME PAGE!');
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF WOOF!');
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000');
+})
