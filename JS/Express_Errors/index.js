12cbdcd47e743a248568fb27c80415d2a8180bb5
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-06 11:54:48 -0400

    express built-in error handler

---
+++ JS/Express_Errors/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/dogs', (req, res, next) => {
+    console.log('I LOVE DOGS!!!')
+    next();
+})
+
+const verifyPassword = ((req, res, next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget') {
+        next();
+    }
+    // res.send('SORRY! YOU NEED A PASSWORD!!!')
+    throw new Error('Password required!')
+})
+
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
+    res.send('HOME PAGE!');
+})
+
+app.get('/error', (req, res) => {
+    chicken.fly();
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF WOOF!');
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!');
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000');
+})
