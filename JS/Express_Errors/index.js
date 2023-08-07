fa6a7eb5da63ff19232dd0e9040ec60c67dc21c8
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-06 23:38:06 -0400

    add custom error class

--- JS/Express_Errors/index.js
+++ JS/Express_Errors/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
+
+const AppError = require('./AppError')
 
 app.use(morgan('dev'));
 
@@ -21,8 +22,9 @@ const verifyPassword = ((req, res, next) => {
     if (password === 'chickennugget') {
         next();
     }
+    throw new AppError('Password Required!', 401);
     // res.send('SORRY! YOU NEED A PASSWORD!!!')
-    throw new Error('Password required!')
+    // throw new AppError(401, 'Password required!')
 })
 
 app.get('/', (req, res) => {
@@ -42,17 +44,26 @@ app.get('/secret', verifyPassword, (req, res) => {
     res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
 })
 
+app.get('/admin', (req, res) => {
+    throw new AppError('Your are not an Admin!', 403);
+})
+
 app.use((req, res) => {
     res.status(404).send('NOT FOUND!');
 })
 
+// app.use((err, req, res, next) => {
+//     console.log("***************************************")
+//     console.log("****************ERROR******************")
+//     console.log("***************************************")
+//     console.log(err)
+//     next(err)
+//     // res.status(500).send("OH BOY! WE GOT AN ERROR!")
+// })
+
 app.use((err, req, res, next) => {
-    console.log("***************************************")
-    console.log("****************ERROR******************")
-    console.log("***************************************")
-    console.log(err)
-    next(err)
-    // res.status(500).send("OH BOY! WE GOT AN ERROR!")
+    const { status = 500, message = 'Something Went Wrong!' } = err;
+    res.status(status).send(message)
 })
 
 app.listen(3000, () => {
