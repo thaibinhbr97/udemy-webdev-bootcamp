811039ec11e03ce0f47930d5a34ff9747bebd98a
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-08 17:10:57 -0400

    defining an async utility

--- JS/Async_Errors/index.js
+++ JS/Async_Errors/index.js
@@ -23,76 +23,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const categories = ['fruit', 'vegetable', 'dairy', 'meat'];
-app.get('/products', async (req, res, err) => {
-    try {
-        const { category } = req.query;
-        if (category) {
-            const products = await Product.find({ category })
-            res.render('products/index.ejs', { products, category })
-        } else {
-            const products = await Product.find({})
-            res.render('products/index.ejs', { products, category: 'All' })
-        }
-    } catch (e) {
-        next(e);
+
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
     }
-})
+}
+
+app.get('/products', wrapAsync(async (req, res, err) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index.ejs', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index.ejs', { products, category: 'All' })
+    }
+}));
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories })
 })
 
-app.post('/products', async (req, res, next) => {
-    try {
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.redirect(`/products/${newProduct._id}`)
-    } catch (e) {
-        next(e);
-    }
-})
+app.post('/products', wrapAsync(async (req, res, next) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+}));
 
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id)
-        if (!product) {
-            throw next(new AppError('Product Not Found', 404));
-        }
-        res.render('products/show', { product })
-    } catch (e) {
-        next(e);
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    if (!product) {
+        throw next(new AppError('Product Not Found', 404));
     }
-})
+    res.render('products/show', { product })
+}));
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        if (!product) {
-            return next(new AppError('Product Not Found', 404));
-        }
-        res.render('products/edit', { product, categories });
-    } catch (e) {
-        next(e);
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        return next(new AppError('Product Not Found', 404));
     }
-})
+    res.render('products/edit', { product, categories });
+}));
 
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-        res.redirect(`/products/${product._id}`);
-    } catch (e) {
-        next(e);
-    }
-})
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+}));
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
-})
+}));
 
 app.use((err, req, res, next) => {
     const { status = 500, message = "Something went wrong!" } = err;
