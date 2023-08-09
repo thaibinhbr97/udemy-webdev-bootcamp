d6ddb10c526a6c34f460c6f10951ebb264940575
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-09 11:13:23 -0400

    basic error handler

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -35,10 +35,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new.ejs');
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -61,6 +65,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+})
+
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!');
 })
 
 app.listen(3000, () => {
