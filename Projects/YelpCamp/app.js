b41a5b13bb8ef559472dd0a35758df8458b12557
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-20 09:02:12 -0400

    creating reviews

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -8,6 +8,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground.js');
+const Review = require('./models/review.js')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -77,6 +78,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
