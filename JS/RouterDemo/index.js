7727db9da97a1f14f8fb47408df581c89848a2ce
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-21 15:19:42 -0400

    express router & middleware

---
+++ JS/RouterDemo/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const shelterRoutes = require('./routes/shelter');
+const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin')
+
+app.use('/shelters', shelterRoutes);
+app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes);
+
+app.listen(3000, () => {
+    console.log('Serving app on localhost:3000');
+})
+
