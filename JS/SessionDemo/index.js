594f6bb09791bb6ba5fc34c92414b1db86bed2d5
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-29 15:46:37 -0400

    express session

---
+++ JS/SessionDemo/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({ secret: 'thisisnotagoodsecret' }));
+
+app.get('/viewcount', (req, res) => {
+    if (res.session.count) {
+        res.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page ${req.session.count} times`);
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
