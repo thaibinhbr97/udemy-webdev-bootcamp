f7a4252a93c66c7542b0a970e706b40c31b8c11e
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-28 23:19:55 -0400

    signing cookies

--- JS/CookiesDemo/index.js
+++ JS/CookiesDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
     const { name = 'No-name' } = req.cookies;
@@ -13,6 +13,17 @@ app.get('/setname', (req, res) => {
     res.cookie('name', 'Steve Chick')
     res.cookie('animal', 'Harlequin Shrimp')
     res.send('OK SENT YOU A COOKIE!!!');
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true });
+    res.send('OK SIGNED YOUR FRUIT COOKIE!');
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send(req.signedCookies);
 })
 
 app.listen(3000, () => {
