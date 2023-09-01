dbbadafedb8e950f4ebf845b2d5e95050165e645
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-08-31 23:11:46 -0400

    Auth Demo: Registering

--- JS/AuthDemo/index.js
+++ JS/AuthDemo/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO! MONGO CONNECTION ERROR!")
+        console.log(err)
+    })
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE!')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    // res.send(req.body);
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {
