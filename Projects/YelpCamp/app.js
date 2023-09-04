0c247e05765347c6c6370ffb5b1c93fe9b4d3346
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-09-04 15:41:26 -0400

    configuring passport

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -6,7 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const Joi = require('joi');
 const methodOverride = require('method-override');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campground.js');
 const reviews = require('./routes/review.js');
@@ -42,10 +44,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser);
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'brad@gmail.com', username: 'brad' });
+    const newUser = await User.register(user, 'monkey');
+    res.send(newUser);
 })
 
 app.use('/campgrounds', campgrounds);
