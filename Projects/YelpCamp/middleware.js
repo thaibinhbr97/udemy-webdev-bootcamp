87b0d5ef3ec51152866325d14719c83796a79854
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-09-08 18:37:30 -0400

    more reviews authorization

--- Projects/YelpCamp/middleware.js
+++ Projects/YelpCamp/middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const ExpressError = require('./utils/ExpressError');
+const ExpressError = require('./utils/ExpressError.js');
 const Campground = require('./models/campground.js');
+const Review = require('./models/review.js')
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -48,4 +49,14 @@ module.exports.validateReview = (req, res, next) => {
     } else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
 }
