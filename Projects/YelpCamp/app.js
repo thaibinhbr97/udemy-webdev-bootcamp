5ff2b11dc32e56885096eb87d4b6c48d54b27217
Brad Nguyen <thaibinhbr97@gmail.com>
2023-09-14 15:45:32 -0400

    Mongo Injection

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -16,6 +16,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campground.js');
 const reviewRoutes = require('./routes/review.js');
@@ -37,6 +39,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    mongoSanitize({
+    replaceWith: '_',
+    })
+);
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
@@ -59,6 +66,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -70,9 +78,6 @@ app.get('/fakeUser', async (req, res) => {
     const newUser = await User.register(user, 'monkey');
     res.send(newUser);
 })
-
-// /regiser - FORM
-// POST /register - create a user
 
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
