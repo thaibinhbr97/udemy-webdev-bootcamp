4b4d0942c9067463ccee8c0c842d965b09e13c75
Brad Nguyen <bradnguyen@Brads-MacBook-Pro.local>
2023-09-13 21:39:50 -0400

    Customizing Map Popup

--- Projects/YelpCamp/public/javascripts/showPageMap.js
+++ Projects/YelpCamp/public/javascripts/showPageMap.js
@@ -1,12 +1,15 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/dark-v11', // style URL
     center: campgroundObj.geometry.coordinates, // starting position [lng, lat]
     zoom: 10, // starting zoom
 });
 
 // Create a default Marker and add it to the map.
-const marker = new mapboxgl.Marker()
+new mapboxgl.Marker()
     .setLngLat(campgroundObj.geometry.coordinates)
-    .addTo(map);
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h3>${campgroundObj.title}</h3><p>${campgroundObj.location}</p>`)) // add popup
+    .addTo(map)
