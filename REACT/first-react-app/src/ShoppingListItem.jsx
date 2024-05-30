4e5e5b8c9ff1f80445c1858b90a1af7a36151d51
Brad Nguyen <thaibinhbr97@gmail.com>
2024-05-30 15:56:25 -0400

    try out PropTypes library

--- REACT/first-react-app/src/ShoppingListItem.jsx
+++ REACT/first-react-app/src/ShoppingListItem.jsx
@@ -1,8 +1,16 @@
+import PropTypes from 'prop-types';
+
 function ShoppingListItem({ name, quantity, completed }) {
     const styles = { color: completed ? "grey" : "red", textDecoration: completed ? "line-through" : "none" };
     return <li style={styles}
     >{name} - {quantity}
     </li>
+}
+
+ShoppingListItem.propTypes = {
+    name: PropTypes.string,
+    quantity: PropTypes.number,
+    completed: PropTypes.bool
 }
 
 export default ShoppingListItem;
