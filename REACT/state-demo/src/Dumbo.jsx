441835eb619bdb7a73a69a83485f9634487ebd95
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-04 00:37:55 -0400

    state initializer functions

---
+++ REACT/state-demo/src/Dumbo.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function generateBoard() {
+    console.log("MAKING THE INITIAL GAME BOARD");
+    return Array(5000);
+}
+
+export default function Dumbo() {
+    const [board, setBoard] = useState(generateBoard);
+    return (
+        <button onClick={() => setBoard("hello")}>Click me to change state</button>
+    )
+}
