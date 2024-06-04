519bc3886a52d7f83eef49b1f420997d4c289943
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-04 14:01:46 -0400

    arrays in state

--- REACT/state-demo/src/App.jsx
+++ REACT/state-demo/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 // import Counter from './Counter'
 // import Dumbo from './Dumbo'
-import ScoreKeeper from './ScoreKeepper'
+// import ScoreKeeper from './ScoreKeepper'
+import EmojiClicker from './EmojiClicker'
 
 function App() {
   return (
     <>
-      <h1>State Demo</h1>
-      <ScoreKeeper />
+      {/* <h1>State Demo</h1> */}
+      <EmojiClicker />
+      {/* <ScoreKeeper /> */}
       {/* <Dumbo /> */}
       {/* <Counter /> */}
     </>
---
+++ REACT/state-demo/src/EmojiClicker.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function EmojiClicker() {
+    const [emojis, setEmojis] = useState(["😁"])
+    const addEmoji = () => {
+        setEmojis(oldEmojis => [...oldEmojis, "😇"]);
+    };
+    return (<div>
+        {emojis.map(e => <span style={{ fontSize: "4rem" }}>{e}</span>)}
+        <button onClick={addEmoji}>Add Emoji</button>
+    </div>);
+}
