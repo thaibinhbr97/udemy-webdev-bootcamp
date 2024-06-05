f10fd089fc649fdad0705b2fe4100dfa6d404205
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-05 00:39:33 -0400

    delete from arrays in react

--- REACT/state-demo/src/EmojiClicker.jsx
+++ REACT/state-demo/src/EmojiClicker.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react';
 import { v4 as uuid } from "uuid";
 
+function randomEmojis() {
+    const choices = ['😀', '🤣', '🥳', '😎', '😤', '🤬', '🤢', '😈', '😵', '🫣', '🙄', '🥱', '😹', '👶🏿', '😿']
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
 export default function EmojiClicker() {
-    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😁" }])
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmojis() }])
     const addEmoji = () => {
-        setEmojis(oldEmojis => [...oldEmojis, { id: uuid(), emoji: "😇" }]);
+        setEmojis(oldEmojis => [...oldEmojis, { id: uuid(), emoji: randomEmojis() }]);
     };
+    const deleteEmoji = (id) => {
+        // delete the emoji with the specified id
+
+        // JS way
+        // const updatedEmojis = emojis.filter(e => e.id !== id);
+        // setEmojis(updatedEmojis)
+
+        // REACT way
+        setEmojis(prevEmojis => {
+            return prevEmojis.filter(e => e.id !== id);
+        })
+    }
     return (<div>
-        {emojis.map(e => <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>)}
+        {emojis.map(e => <span onClick={() => deleteEmoji(e.id)} key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>)}
         <button onClick={addEmoji}>Add Emoji</button>
     </div>);
 }
