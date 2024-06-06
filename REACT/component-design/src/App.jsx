5f79514e58545a1d73b63ad824e8c82a71db0bd1
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-06 13:59:37 -0400

    formulating our component gameplan

--- REACT/component-design/src/App.jsx
+++ REACT/component-design/src/App.jsx
@@ -6,6 +6,11 @@ import './App.css'
 // shows a win message when total equals goal
 // a "roll again" button that re-rolls all the dice
 
+// lift the state as high as possible - but not higher
+// App -> LuckyN -> Dice -> Die, Die
+// Generally, components are one of two types: presentational (UI) and logical (state or logic)
+// App, Dice, Die, Die: presentational, LuckyN: logical
+
 
 function App() {
 
