90735934da66d87ff54b47b2508d5ba883bd746a
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-11 14:42:25 -0400

    the htmlFor property

--- REACT/react-forms/src/UsernameForm.jsx
+++ REACT/react-forms/src/UsernameForm.jsx
@@ -5,7 +5,8 @@ export default function UsernameForm() {
     const updateUsername = (evt) => setUsername(evt.target.value);
     return (
         <div>
-            <input type="text" placeholder="username" value={username} onChange={updateUsername} />
+            <label htmlFor="username">Enter a username</label>
+            <input id="username" type="text" placeholder="username" value={username} onChange={updateUsername} />
             <button>Submit</button>
         </div>
     )
