719f31df88c1077447a69dd68d6b5f4f7596159a
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-17 10:11:32 -0400

    play around with the rating component

--- REACT/material-ui-demo/src/App.jsx
+++ REACT/material-ui-demo/src/App.jsx
@@ -1,19 +1,21 @@
-import AlarmIcon from '@mui/icons-material/Alarm';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+// import AlarmIcon from '@mui/icons-material/Alarm';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
 import './App.css';
+import RatingDemo from './RatingDemo';
 
 function App() {
 
   return (
     <>
-      <Button variant="contained" onClick={() => alert('HI!')}>Contained</Button>
+      {/* <Button variant="contained" onClick={() => alert('HI!')}>Contained</Button>
       <Button size="small" variant="text">Text</Button>
       <Button variant="outlined">Outlined</Button>
       <Button size="large" color="success" variant="contained" onClick={() => alert('HI!')}>Contained</Button>
       <IconButton color="secondary" aria-label="add an alarm">
         <AlarmIcon />
-      </IconButton>
+      </IconButton> */}
+      <RatingDemo />
     </>
   )
 }
