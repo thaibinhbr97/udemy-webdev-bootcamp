1432d8d0e3ee18ccf4cdc9a6904718c38aa27166
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-17 09:52:17 -0400

    installing material ui and try out different buttons and icon buttons

---
+++ REACT/material-ui-demo/src/App.jsx
@@ -0,0 +1,21 @@
+import AlarmIcon from '@mui/icons-material/Alarm';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained" onClick={() => alert('HI!')}>Contained</Button>
+      <Button size="small" variant="text">Text</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button size="large" color="success" variant="contained" onClick={() => alert('HI!')}>Contained</Button>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+    </>
+  )
+}
+
+export default App
