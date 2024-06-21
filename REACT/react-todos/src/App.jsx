34c1011348369597da08d3b205557aa28c1e399a
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-21 00:46:06 -0400

    The TodoList component

---
+++ REACT/react-todos/src/App.jsx
@@ -0,0 +1,17 @@
+import CssBaseline from "@mui/material/CssBaseline"
+import './App.css'
+import TodoList from "./TodoList"
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+      <TodoList />
+    </>
+  )
+}
+
+export default App
+
