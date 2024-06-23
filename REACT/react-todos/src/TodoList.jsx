404f7d3d86580e9f4ee81fc84208e16c0f93fe0a
Brad Nguyen <thaibinhbr97@gmail.com>
2024-06-23 16:56:45 -0400

    removing Todos

--- REACT/react-todos/src/TodoList.jsx
+++ REACT/react-todos/src/TodoList.jsx
@@ -1,11 +1,5 @@
-import CommentIcon from '@mui/icons-material/Comment';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import TodoItem from './TodoItem';
 
 import { useState } from 'react';
 
@@ -18,37 +12,17 @@ const initialTodos = [
 
 export default function TodoList() {
     const [todos, setTodos] = useState(initialTodos);
+    const removeTodo = (id) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter((t) => t.id !== id);
+        })
+    }
+
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {todos.map((todo) => {
-                const labelId = `checkbox-list-label-${todo.id}`;
-
-                return (
-                    <ListItem
-                        key={todo.id}
-                        secondaryAction={
-                            <IconButton edge="end" aria-label="comments">
-                                <CommentIcon />
-                            </IconButton>
-                        }
-                        disablePadding
-                    >
-                        <ListItemButton role={undefined} dense>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={todo.completed}
-                                    tabIndex={-1}
-                                    disableRipple
-                                    inputProps={{ 'aria-labelledby': labelId }}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id={labelId} primary={todo.text} />
-                        </ListItemButton>
-                    </ListItem>
-                );
-
-            })}
+            {todos.map((todo) => (
+                <TodoItem todo={todo} key={todo.id} removeTodo={() => removeTodo(todo.id)} />
+            ))}
         </List >
     )
 }
